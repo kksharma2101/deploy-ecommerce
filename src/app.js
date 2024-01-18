13bde5a6997afe45cbfbca0419aa9956cfde8563
kamal sharma <kamalbsr2101@gmail.com>
2024-01-18 21:54:22 +0530

    add path and filename and dirname

--- src/app.js
+++ src/app.js
@@ -6,23 +6,28 @@ import connectDb from "../config/db.config.js";
 import authRouter from "../routers/auth.router.js";
 import categoryRouter from "../routers/categoryRoutes.js";
 import productRouter from "../routers/productRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middleware call
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://e-commerce-gset6bwru-kamal-sharmas-projects.vercel.app",
-  })
-);
+app.use(cors());
+app.use(express.static(path.join(__dirname, "./build")));
 
 // router call
 app.use("/api", authRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
+
+app.use("", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
 
 // call database
 connectDb();
