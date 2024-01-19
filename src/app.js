3b7a8802fb84e5eade93e242f6c0da795a68745a
kamal sharma <kamalbsr2101@gmail.com>
2024-01-19 12:23:28 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // router call
 app.use("/api", authRouter);
@@ -26,7 +26,7 @@ app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // call database
